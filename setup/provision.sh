7b25dd572023c965a2c7c56a8763b1b0bf92207e
Riccardo Tempesta <info@riccardotempesta.com>
2016-10-26 23:37:25 +0200

    Added python mumpy package to setup

--- setup/provision.sh
+++ setup/provision.sh
@@ -6,6 +6,7 @@ apt-get upgrade
 apt-get install python3 python3-yaml screen python-opencv festival festvox-italp16k festvox-itapc16k
 pip3 install pyserial
 pip3 install imutils
+pip3 install numpy
 pip3 install PySocks
 pip3 install python-telegram-bot
 pip3 install python-telegram-bot --upgrade
